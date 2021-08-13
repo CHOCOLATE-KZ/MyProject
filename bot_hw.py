9fb6e0cf0ef1b67d9c0cd9d65e302c2711590f7d
CHOCOLATE-KZ <78365136+CHOCOLATE-KZ@users.noreply.github.com>
2021-08-13 13:56:59 +0500

    Add files via upload
    Фото нах-ся в той же папке, где и файл.py

---
+++ bot_hw.py
@@ -0,0 +1,21 @@
+#д.з отправляем у меня сегодня день рождение | бот отправляет открытку
+#отправляем мне грустно, он пишет не грусти
+
+from telebot import *
+
+
+TOKEN = "1934658582:AAFleJFHFNuaZ3ZVtbxrt1io6WpmUI2a7mk"
+bot = TeleBot(TOKEN)
+
+@bot.message_handler(content_types=['text'])
+def no_sad(message):
+    if message.text == "мне грустно" or message.text == "Мне грустно":
+        bot.send_message(message.chat.id, "Не грусти, всё будет хорошо)☺️")
+
+@bot.message_handler(content_types=['image'])
+def send_img(message):
+    if message.text == "др":
+        photo = open("birthday-1061.jpg", 'rb')
+        bot.send_photo(message.chat.id, photo)
+
+bot.polling()
